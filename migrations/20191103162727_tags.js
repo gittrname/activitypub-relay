c779eedfa7598553ee2b45b00e0f6b0b77e769a7
trname <txgfx504@yahoo.co.jp>
2019-11-11 21:36:38 +0900

    tags migration add

---
+++ migrations/20191103162727_tags.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex, Promise) {
+  
+  return knex.schema.hasTable('tags').then(function(exists) {
+    if (!exists) {
+      return knex.schema.createTable('tags', function(table) {
+        table.increments('id').primary();
+        table.string('type', 128);
+        table.string('href', 2048);
+        table.string('name', 256);
+
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+      });
+    }else{
+      return new Error("Table:tags is already exists");
+    }
+  });
+};
+
+exports.down = function(knex, Promise) {
+
+  return knex.schema.hasTable('tags').then(function(exists) {
+    if (exists) {
+      return knex.schema.dropTable('tags');
+    }
+  });
+};
