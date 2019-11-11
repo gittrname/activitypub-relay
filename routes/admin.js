7303ea3cea87f43e318d53b50c372b02456d331a
trname <txgfx504@yahoo.co.jp>
2019-11-11 21:39:18 +0900

    add tags page

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,7 @@ var database = require('../database');
 var router = express.Router();
 
 require('knex-paginator')(database);
+require('date-utils');
 
 // 設定ロード
 var config = require('../settings');
@@ -75,6 +76,28 @@ router.use("/accounts/:page?", isAuthenticated, function(req, res, next) {
     .paginate(20, page, true)
     .then(function(result) {
       res.render("admin/account", {'keyword': keyword, 'result': result});
+    })
+    .catch(function(err) {
+      next(err);
+    });
+});
+
+//
+// Tagページ
+router.use("/tags/:page?", isAuthenticated, function(req, res, next) {
+
+  var page = req.param('page', 1);
+  var keyword = req.param('k', "");
+
+  database('tags')
+    .select('name')
+    .count({count: 'name'})
+    .max({last_use: 'updated_at'})
+    .where('name', 'like', "%" + keyword + "%")
+    .groupBy('name')
+    .paginate(20, page, true)
+    .then(function(result) {
+      res.render("admin/tag", {'keyword': keyword, 'result': result});
     })
     .catch(function(err) {
       next(err);
