5551889e94891c47bc4306575913ce9e6b5c5d7d
ranpei <ranpei@ranran.mydns.jp>
2022-11-24 18:23:20 +0900

    トップページに参加サーバー一覧を追加

--- routes/ui.js
+++ routes/ui.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var database = require('../database');
 var router = express.Router();
 
 //
 // Topページ
 router.get('/', function (req, res, next) {
-  res.render("top");
+
+  database('relays')
+    .then(function(instances) {
+      res.render("top", {'instances': instances});
+    })
 });
 
 //
